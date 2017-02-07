f0aba447fc481d588174055ad4b28a075e97e0fc
Evagelos <ev-agelos@users.noreply.github.com>
2017-02-07 10:17:16 +0100

    better defaults, add background opacity

--- ui_elements.py
+++ ui_elements.py
@@ -3,9 +3,9 @@ import ac
 
 class UIElement:
 
-    def __init__(self, window, text='', size=(0, 0), pos=(0, 0),
-                 font_color=(0, 0, 0, 0), font_size=12, bg_color=(0, 0, 0),
-                 draw_bg=0, draw_border=0):
+    def __init__(self, window, text='', size=(20, 20), pos=(0, 0),
+                 font_color=(1, 1, 1, 1), font_size=12, bg_color=(1, 1, 1),
+                 draw_bg=1, bg_opacity=0.6, draw_border=0):
         self.id = None
         self.window = window
         self.text = text
@@ -14,16 +14,17 @@ class UIElement:
         self.font_color = font_color
         self.font_size = font_size
         self.bg_color = bg_color
-        self.draw_bg = draw_bg
+        self.draw_bg = draw_bg  # 0 (transparent) or 1 (not trasparent)
+        self.bg_opacity = bg_opacity  # 0 <= float <= 1
         self.draw_border = draw_border
 
     def draw(self):
-        # FIXME draw if element has value otherwise it will throw ERROR!
         ac.setSize(self.id, *self.size)
         ac.setPosition(self.id, *self.pos)
         ac.setFontColor(self.id, *self.font_color)
         ac.setFontSize(self.id, self.font_size)
         ac.setBackgroundColor(self.id, *self.bg_color)
+        ac.setBackgroundOpacity(self.id, self.bg_opacity)
         ac.drawBackground(self.id, self.draw_bg)
         ac.drawBorder(self.id, self.draw_border)
 
