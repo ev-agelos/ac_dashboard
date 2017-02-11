54c0ed70c737c2c81ebc08d46aa580b4da20d3d8
Evagelos <ev-agelos@users.noreply.github.com>
2017-02-11 21:39:34 +0100

    fix dashboard, decoupled code

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,10 @@
 """Main dashboard that shows the car's info about speed, gears times etc."""
 
 from itertools import cycle
-
 import ac
-
 from convert_time import int_to_time
-from ui_elements import UIProgressBar, UILabel, UIButton
+from ui_elements import (UIProgressBar, UILabel, UIButton, Led, RedLed, GreenLed,
+                         BlueLed)
 
 
 class DashBoard:
@@ -36,212 +35,248 @@ class DashBoard:
         self.data_queue = []
 
 
+class Speedometer:
+
+    def __init__(self, dashboard):
+        self.dashboard = dashboard
+        for telemetry in ('in_pits', 'rpm'):
+            self.dashboard.subscribe(telemetry, self)
+        self.f1_style = False
+        self.leds = {
+            'green': [GreenLed(pos_x=Led.pos_x + (number*20))
+                      for number in range(5)],
+            'red':  [RedLed(pos_x=Led.pos_x + (number*20))
+                     for number in range(5, 10)],
+            'blue': [BlueLed(pos_x=Led.pos_x + (number*20))
+                     for number in range(10, 15)]
+        }
+        self.leds_counter = len([led for group in self.leds.values()
+                                 for led in group])
+        self.lock = False
+
+    def run(self, telemetry, value):
+        if telemetry == 'rpm':
+            rpm = value['current']
+            max_rpm = value['max']
+            if self.lock or rpm > max_rpm:
+                self._draw_all()
+            elif rpm < 10:  # no need to render really low rpms
+                pass
+            else:
+                self._draw(rpm, max_rpm)
+        elif telemetry == 'in_pits':
+            self.lock = value
+
+    def _draw_all(self):
+        for color_group in self.leds.values():
+            for led in color_group:
+                led.draw()
+
+    def _draw(self, rpm, max_rpm):
+        if self.f1_style:
+            for i in range(0, round(rpm * 3 / max_rpm)):
+                if i == 0:
+                    for led in self.leds['green']:
+                        led.draw()
+                elif i == 1 and rpm > 4500:
+                    for led in self.leds['red']:
+                        led.draw()
+                elif i == 2 and rpm > 6300:
+                    for led in self.leds['blue']:
+                        led.draw()
+        else:
+            for i in range(0, round(rpm * self.leds_counter / max_rpm)):
+                if i < 5:
+                    color = 'green'
+                    led_index = i
+                elif i < 10:
+                    color = 'red'
+                    led_index = i - 5
+                else:
+                    color = 'blue'
+                    led_index = i -10
+                self.leds[color][led_index].draw()
+
+
 class FuelBar(UIProgressBar):
 
-    def __init__(self, window, dashboard):
-        super().__init__(
-            window, size=(65, 17), pos=(181, 105), font_color=(1, 0.56, 0, 1),
-            bg_color=(1, 1, 0), bg_opacity=0.4, draw_border=0)
-
+    def __init__(self, dashboard, *args):
+        super().__init__(*args, size=(78, 19), pos=(181, 106),
+                         font_color=(1, 0.56, 0, 1), bg_color=(1, 1, 0),
+                         bg_opacity=0.4, draw_border=0)
         self.dashboard = dashboard
-        self.pit_limiter_flag = None
-        for telemetry in ('max_fuel', 'fuel', 'pit_limiter',
-                          'pit_limiter_flag'):
-            self.dashboard.subscribe(telemetry, self)
+        self.dashboard.subscribe('fuel_percent', self)
 
     def run(self, telemetry, value):
         """Update the labels with the given value."""
-        if telemetry == 'max_fuel':
-            ac.setRange(self.id, 0, value)
-            self.dashboard.unsubscribe(telemetry, self)
-        elif telemetry == 'fuel':
-            ac.setValue(self.id, round(value))
-        elif telemetry == 'pit_limiter':
-            if value > 0:
-                self.hide()
-            elif value == 0 and self.pit_limiter_flag is True:
-                self.show()
-        elif telemetry == 'pit_limiter_flag':
-            self.pit_limiter_flag = value
+        self.percent = value  # only update fuel_percent
 
 
 class FuelButton(UIButton):
 
-    modes = cycle([
-        ('fuel_percent', ),
-        ('burned_fuel', ),
-        ('fuel_laps_left', )
-    ])
+    modes = cycle(['fuel_percent', 'burned_fuel', 'fuel_laps_left'])
 
-    def __init__(self, window, dashboard, **kwargs):
-        super().__init__(window, text='/', size=(65, 17), pos=(181, 105),
-                         font_color=(0, 0, 0, 1), draw_border=1,
-                         bg_opacity=0, **kwargs)
+    def __init__(self, dashboard):
+        super().__init__(fuel_click, text='/', size=(78, 19), pos=(181, 106),
+                         font_color=(0, 0, 0, 1), draw_border=1, bg_opacity=0)
         self.dashboard = dashboard
         self.mode = None
         self.switch_mode()
 
     def switch_mode(self):
         if self.mode is not None:
-            for telemetry in self.mode:
-                self.dashboard.unsubscribe(telemetry, self)
+            self.dashboard.unsubscribe(self.mode, self)
         self.mode = next(self.modes)
-        for telemetry in self.mode:
-            self.dashboard.subscribe(telemetry, self)
+        self.dashboard.subscribe(self.mode, self)
 
     def run(self, telemetry, value):
         if telemetry == 'burned_fuel':
-            text = '{0:.1f}L'.format(value) if value is not None else ''
-            ac.setText(self.id, 'Pre: ' + text)
+            text = '{0:.1f}L'.format(value)
+            self.text = 'Pre: ' + text
         elif telemetry == 'fuel_laps_left':
-            ac.setText(self.id, 'Laps: {}'.format(value))
+            self.text = 'Laps: {}'.format(value)
         elif telemetry == 'fuel_percent':
-            value = round(value, 1) if value is not None else ''
-            ac.setText(self.id, str(value) + '%')
+            self.text = str(round(value, 1)) + '%'
 
 
 class GearLabel(UILabel):
 
-    def __init__(self, window, dashboard):
-        super().__init__(window, pos=(290, 58),
-                                      font_color=(1, 0, 0, 1), font_size=40)
+    def __init__(self, dashboard):
+        super().__init__(pos=(288, 55), size=(40, 55), bg_opacity=0,
+                         font_color=(1, 0, 0, 1), font_size=40)
         self.dashboard = dashboard
         self.dashboard.subscribe('gear', self)
 
     def run(self, telemetry, value):
-        ac.setText(self.id, str(value))
+        self.text = str(value)
 
 
 class SpeedRpmButton(UIButton):
 
-    modes = cycle([
-        ('speed', ),
-        ('max_speed', ),
-        ('rpm', )
-    ])
+    modes = cycle(['speed', 'max_speed', 'rpm'])
 
-    def __init__(self, window, dashboard, **kwargs):
-        super().__init__(window, pos=(365, 70), size=(80, 30),
-                                       font_size=25, **kwargs)
+    def __init__(self, dashboard):
+        super().__init__(rpm_speed_click, pos=(355, 67), size=(80, 35),
+                         font_color=(1, 0, 0, 1), font_size=25)
         self.dashboard = dashboard
+        self.dashboard.subscribe('in_pits', self)
         self.mode = None
         self.switch_mode()
 
     def switch_mode(self):
         if self.mode is not None:
-            for telemetry in self.mode:
-                self.dashboard.unsubscribe(telemetry, self)
+            self.dashboard.unsubscribe(self.mode, self)
         self.mode = next(self.modes)
-        for telemetry in self.mode:
-            self.dashboard.subscribe(telemetry, self)
+        self.dashboard.subscribe(self.mode, self)
 
     def run(self, telemetry, value):
         if telemetry == 'max_speed':
-            ac.setFontColor(self.id, 0.5, 0, 1, 1)
+            self.font_color = (0.5, 0, 1, 1)
+            self.text = str(round(value))
+        elif telemetry == 'speed':
+            self.font_color = (1, 0, 0, 1)
+            self.text = str(round(value))
         elif telemetry == 'rpm':
-            ac.setFontColor(self.id, 1, 0, 0, 1)
-        ac.setText(self.id, str(round(value)))
+            self.font_color = (1, 0, 0, 1)
+            self.text = str(round(value['current']))
+        elif telemetry == 'in_pits':
+            if value is True:
+                self.dashboard.unsubscribe(self.mode, self)
+                self.text = 'IN PITS'
+                self.font_color = (1, 0, 0, 1)
+            else:
+                self.dashboard.subscribe(self.mode, self)
+            self.lock = value
 
 
 class TimesButton(UIButton):
 
-    modes = cycle([
-        ('pb', ),
-        ('theoretical_best', )
-    ])
+    modes = cycle(['pb', 'theoretical_best'])
 
-    def __init__(self, window, dashboard, **kwargs):
-        super().__init__(window, pos=(270, 104), size=(80, 20),
-                                       font_size=15, **kwargs)
+    def __init__(self, dashboard):
+        super().__init__(times_click, pos=(268, 105), size=(80, 20),
+                         font_size=15)
         self.dashboard = dashboard
+        self.dashboard.subscribe('in_pits', self)
         self.mode = None
         self.switch_mode()
 
     def switch_mode(self):
         if self.mode is not None:
-            for telemetry in self.mode:
-                self.dashboard.unsubscribe(telemetry, self)
+            self.dashboard.unsubscribe(self.mode, self)
         self.mode = next(self.modes)
-        for telemetry in self.mode:
-            self.dashboard.subscribe(telemetry, self)
+        self.dashboard.subscribe(self.mode, self)
 
     def run(self, telemetry, value):
         if telemetry == 'pb':
             colors = (1, 0, 0, 1)
+            time = int_to_time(value) if value is not None else '--:--:--'
+            self.text = str(time)
+            self.font_color = colors
         elif telemetry == 'theoretical_best':
             colors = (0.5, 0, 1, 1)
-        time = int_to_time(value) if value is not None else '--:--:--'
-        ac.setText(self.id, str(time))
-        ac.setFontColor(self.id, *colors)
+            time = int_to_time(value) if value is not None else '--:--:--'
+            self.text = str(time)
+            self.font_color = colors
+        elif telemetry == 'in_pits':
+            if value is True:
+                self.dashboard.unsubscribe(self.mode, self)
+                self.text = '--:--:--'
+            else:
+                self.dashboard.subscribe(self.mode, self)
+            self.lock = value
 
 
 class PosLapsButton(UIButton):
 
-    modes = cycle([
-        ('total_laps', 'laps_counter'),
-        ('position', 'num_cars')
-    ])
+    modes = cycle(['laps', 'position'])
 
-    def __init__(self, window, dashboard, **kwargs):
-        super().__init__(window, text='/', pos=(163, 70),
-                                       size=(80, 30), font_size=25,
-                                       **kwargs)
+    def __init__(self, dashboard):
+        super().__init__(pos_laps_click, text='/', pos=(180, 67),
+                         size=(80, 33), text_align='left', font_size=25)
         self.dashboard = dashboard
+        self.dashboard.subscribe('in_pits', self)
         self.mode = None
         self.switch_mode()
 
     def switch_mode(self):
         if self.mode is not None:
-            for telemetry in self.mode:
-                self.dashboard.unsubscribe(telemetry, self)
+            self.dashboard.unsubscribe(self.mode, self)
         self.mode = next(self.modes)
-        for telemetry in self.mode:
-            self.dashboard.subscribe(telemetry, self)
+        self.dashboard.subscribe(self.mode, self)
 
     def run(self, telemetry, value):
-        if telemetry in ('total_laps', 'laps_counter'):
-            prefix = 'L: '
-        elif telemetry in ('position', 'num_cars'):
-            prefix = 'P: '
-
-        if telemetry == 'total_laps':
-            _, laps_counter = ac.getText(self.id).lstrip('L: ').split('/')
-            text = str(value + 1) + '/' + laps_counter
-        elif telemetry == 'laps_counter':
-            total_laps, _ = ac.getText(self.id).lstrip('L: ').split('/')
-            text = total_laps + '/' + str(value)
-        elif telemetry == 'num_cars':
-            position, _ = ac.getText(self.id).lstrip('P: ').split('/')
-            text = position + '/' + str(value)
+        if telemetry == 'laps':
+            text = " L: {total_laps}/{laps_counter}"
+            self.text = text.format(**value)
         elif telemetry == 'position':
-            _, num_cars = ac.getText(self.id).lstrip('P: ').split('/')
-            text = str(value) + '/' + num_cars
-
-        ac.setText(self.id, prefix + text)
+            text = " P: {car_position}/{total_cars}"
+            self.text = text.format(**value)
+        elif telemetry == 'in_pits':
+            if value is True:
+                self.dashboard.unsubscribe(self.mode, self)
+            else:
+                self.dashboard.subscribe(self.mode, self)
+            self.lock = value
 
 
 class SectorButton(UIButton):
 
-    modes = cycle([
-        ('performance_meter', ),
-        ('last_sector', )
-    ])
+    modes = cycle(['performance_meter', 'last_sector'])
 
-    def __init__(self, window, dashboard, **kwargs):
-        super().__init__(window, pos=(365, 104),
-                                       size=(80, 20), font_size=15,
-                                       **kwargs)
+    def __init__(self, dashboard, *args):
+        super().__init__(sector_click, *args, pos=(355, 105), size=(80, 20),
+                         font_size=15)
         self.dashboard = dashboard
+        self.dashboard.subscribe('in_pits', self)
         self.mode = None
         self.switch_mode()
 
     def switch_mode(self):
         if self.mode is not None:
-            for telemetry in self.mode:
-                self.dashboard.unsubscribe(telemetry, self)
+            self.dashboard.unsubscribe(self.mode, self)
         self.mode = next(self.modes)
-        for telemetry in self.mode:
-            self.dashboard.subscribe(telemetry, self)
+        self.dashboard.subscribe(self.mode, self)
 
     def run(self, telemetry, value):
         if telemetry == 'performance_meter':
@@ -251,9 +286,52 @@ class SectorButton(UIButton):
                 sector_time = '+' + sector_time
             else:
                 colors = (0, 1, 0, 1)
-            ac.setFontColor(self.id, *colors)
-            ac.setText(self.id, sector_time)
+            self.font_color = colors
+            self.text = sector_time
         elif telemetry == 'last_sector':
             colors = (0, 1, 0, 1) if value['is_pb'] is True else (1, 1, 0, 1)
-            ac.setFontColor(self.id, *colors)
-            ac.setText(self.id, str(value['time']))
+            self.font_color = colors
+            self.text = str(value['time'])
+        elif telemetry == 'in_pits':
+            if value is True:
+                self.dashboard.unsubscribe(self.mode, self)
+                self.text = '--:--:--'
+            else:
+                self.dashboard.subscribe(self.mode, self)
+            self.lock = value
+
+
+def sector_click(x, y):
+    if not SECTOR_BUTTON.lock:
+        SECTOR_BUTTON.switch_mode()
+
+
+def fuel_click(x, y):
+    if not FUEL_BUTTON.lock:
+        FUEL_BUTTON.switch_mode()
+
+
+def times_click(x, y):
+    if not TIMES_BUTTON.lock:
+        TIMES_BUTTON.switch_mode()
+
+
+def rpm_speed_click(x, y):
+    if not SPEED_RPM_BUTTON.lock:
+        SPEED_RPM_BUTTON.switch_mode()
+
+
+def pos_laps_click(x, y):
+    if not POS_LAPS_BUTTON.lock:
+        POS_LAPS_BUTTON.switch_mode()
+
+
+DASHBOARD = DashBoard()
+FUEL_BAR = FuelBar(DASHBOARD)
+FUEL_BUTTON = FuelButton(DASHBOARD)
+GEAR_LABEL = GearLabel(DASHBOARD)
+SPEED_RPM_BUTTON = SpeedRpmButton(DASHBOARD)
+TIMES_BUTTON = TimesButton(DASHBOARD)
+POS_LAPS_BUTTON = PosLapsButton(DASHBOARD)
+SECTOR_BUTTON = SectorButton(DASHBOARD)
+SPEEDOMETER = Speedometer(DASHBOARD)
