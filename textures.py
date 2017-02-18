0986291370c68928d01d098b86abe6a991caa6bd
Evagelos <ev-agelos@users.noreply.github.com>
2017-02-18 10:45:11 +0100

    decouple info_app

--- textures.py
+++ textures.py
@@ -5,60 +5,20 @@ import ac
 
 class Texture:
 
-    id = None
-    filename = ''
     images_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                               'Images')
 
-    @classmethod
-    def _load_texture(cls):
-        if cls.id is None:
-            cls.id = ac.newTexture(os.path.join(cls.images_dir, cls.filename))
-
-    @classmethod
-    def _draw_texture(cls, led):
-        ac.glQuadTextured(led.pos_x, led.pos_y, led.width, led.height, cls.id)
-
-
-class Led(Texture):
-
-    pos_x = 144
-    pos_y = 40
-    width = 32
-    height = 32
-
-    def __init__(self, pos_x=None, pos_y=None, width=None, height=None):
-        self.pos_x = pos_x or self.pos_x
-        self.pos_y = pos_y or self.pos_y
-        self.width = width or self.width
-        self.height = height or self.height
-        super()._load_texture()
+    def __init__(self, pos_x=None, pos_y=None, width=None, height=None,
+                 color=None, filename=''):
+        self.id = ac.newTexture(os.path.join(self.images_dir, filename))
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.width = width
+        self.height = height
+        self.color = color
 
     def draw(self):
-        super()._draw_texture(self)
-
-
-class RedLed(Led):
-
-    filename = 'LedRed.png'
-    pos_y = Led.pos_y + 1
-
-
-class GreenLed(Led):
-
-    filename = 'LedGreen.png'
-
-
-class BlueLed(Led):
-
-    filename = 'LedBlue.png'
-    pos_y = Led.pos_y + 1
-
-
-class YellowLeds(Led):
-
-    filename = 'LedsYellow.png'
-    pos_x = Led.pos_x - 15
-    pos_y = Led.pos_y + 27
-    width = Led.width + 311
-    height = Led.height + 6
+        if self.color is not None:
+            ac.glColor4f(*self.color)
+        ac.glQuadTextured(self.pos_x, self.pos_y, self.width, self.height,
+                          self.id)
